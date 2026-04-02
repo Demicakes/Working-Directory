0d411573140e584c2ecb8db653a2530cb4428068
Demi Parker <47190113+Demicakes@users.noreply.github.com>
2026-04-02 14:15:07 +0100

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,36 @@
+productlist = ["Coffee"]
+
+print("........................")
+print()
+print(productlist)
+print()
+print("........................")
+
+newproduct = str(input(f'Please add a new product: '))
+
+productlist.append(newproduct)
+
+print("........................")
+print()
+print(productlist)
+print()
+print("........................")
+print()
+
+
+
+#for loop
+
+j=0                                 # j is used as a place holder to add +1 every loop
+
+for i in productlist:               # create the for loop and create a variable called i
+    print(j, i, sep =" ")           # the loop will run for each item in productlist (in this case 1) 
+    j+=1                            # this adds + 1 to j every itteration
+
+
+
+
+
+print()
+print("........................")
+
